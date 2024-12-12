021e78739b06c472956c013825f9eac720c85508
Russell Harrower <russell@drn1.com.au>
2024-12-12 17:42:38 +0800

    Added footer - fix speling

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import { useRouter } from "next/router";
 import '../styles/globals.css';
 import Header from '../components/global/header';
-import Footer from '../components/global/fotter';
+import Footer from '../components/global/footer';
 import IdleLogout from "../components/system/idleLogout";
 
 
